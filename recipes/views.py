721668388d9b6411f53c5307411b1f2d9f67f21f
Jonas Farias <tinhanadamelhor@gmail.com>
2024-04-13 18:42:33 -0300

    Removido Contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 
@@ -8,10 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': ' JONAS',
         })
-
-
-def contato(request):
-    return HttpResponse('Oi Contato')
-
-def sobre(request):
-    return HttpResponse('OI Sobre')
