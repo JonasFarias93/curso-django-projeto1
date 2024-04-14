646f0c3fde396b2145c15032ac83af15dea97ed7
Jonas Farias <tinhanadamelhor@gmail.com>
2024-04-13 21:16:58 -0300

    adicionando os arquivos vercel.json

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': ' JONAS',
         })
+
+
